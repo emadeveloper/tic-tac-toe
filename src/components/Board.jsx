02dbe9ab0795a42b85e7b972ccb78f6bbf4df237
Emanuel Martinez <yinproducciones@gmail.com>
2023-12-19 19:18:20 -0300

    add:square logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,20 +1,87 @@
-import React, {useState} from 'react'
-import './Board.css'
-import Square from './Square'
+import React, { useState } from "react";
+import "./Board.css";
+import Square from "./Square";
 
 const Board = () => {
-    const [board,setBoard] = useState (['','','','','','','','',''])
-  return (
-    <div className='board'>
-        <div className="row">
-            <Square value={board[0]} chooseSquare={() => {alert(0)}} />
-            <Square value={board[1]} chooseSquare={() => {alert(1)}} />
-            <Square value={board[2]} chooseSquare={() => {alert(2)}} />
-        </div>
-        <div className="row"></div>
-        <div className="row"></div>
-    </div>
-  )
-}
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X");
 
-export default Board
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((val, idx) => {
+        if (idx === square && val === "") {
+          return player;
+        }
+        return val;
+      })
+    );
+    setPlayer(player === "X" ? "O" : "X");
+  };
+
+  return (
+    <div className="board">
+      <div className="row">
+        <Square
+          value={board[0]}
+          chooseSquare={() => {
+            chooseSquare(0);
+          }}
+        />
+        <Square
+          value={board[1]}
+          chooseSquare={() => {
+            chooseSquare(1);
+          }}
+        />
+        <Square
+          value={board[2]}
+          chooseSquare={() => {
+            chooseSquare(2);
+          }}
+        />
+      </div>
+      <div className="row">
+        <Square
+          value={board[3]}
+          chooseSquare={() => {
+            chooseSquare(0);
+          }}
+        />
+        <Square
+          value={board[4]}
+          chooseSquare={() => {
+            chooseSquare(1);
+          }}
+        />
+        <Square
+          value={board[5]}
+          chooseSquare={() => {
+            chooseSquare(2);
+          }}
+        />
+      </div>
+      <div className="row">
+        <Square
+          value={board[6]}
+          chooseSquare={() => {
+            chooseSquare(0);
+          }}
+        />
+        <Square
+          value={board[7]}
+          chooseSquare={() => {
+            chooseSquare(1);
+          }}
+        />
+        <Square
+          value={board[8]}
+          chooseSquare={() => {
+            chooseSquare(2);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
